e035ed09afbf8ad81f25f3dc84a96dff9d283329
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 19:17:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   string_array = string.split(" ")
   reversed = [] 
  string_array.each do |word| 
-   reversed.unshift(word)
+   reversed << word.reverse
   end 
   reversed.join("")
 end 
